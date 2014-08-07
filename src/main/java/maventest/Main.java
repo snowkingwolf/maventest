08f0014528949e3fd67592b1cf2841da03f4a986
yy_feng <yy_feng@Ctrip.com>
2014-08-07 15:16:49 +0800

    new

---
+++ src/main/java/maventest/Main.java
@@ -0,0 +1,13 @@
+package maventest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void sayHello(String name){
+		System.out.println(name+", hello!");
+	}
+}
